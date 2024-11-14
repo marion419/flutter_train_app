e977f105de9cf3819dcd6dc95f332f7ee0a1be2b
marion419 <daynvi@gmail.com>
2024-11-14 19:14:56 +0900

    패딩 설정

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -12,24 +12,42 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         centerTitle: true,
         title: Text('기차 예매'),
       ),
-      body: Column(
-        children: [
-          SizedBox(height: 200),
-          Center(
-            child: Container(
-              width: 200,
-              height: 100,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Theme.of(context).highlightColor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(height: 100),
+            Center(
+              child: Container(
+                width: double.infinity,
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white,
+                ),
               ),
             ),
-          )
-        ],
+            SizedBox(height: 20,),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: (){}, 
+                child: 
+                  Text('좌석 선택',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
