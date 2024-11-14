42a7730f8d82447fb938250f69e0207eeeab1372
marion419 <daynvi@gmail.com>
2024-11-14 20:44:35 +0900

    StationListPage의 title이 매개변수 문자열을 반영하도록 변경

--- lib/pages/station_list/station_list_page.dart
+++ lib/pages/station_list/station_list_page.dart
@@ -3,11 +3,16 @@
 import 'package:flutter/material.dart';
 
 class StationListPage extends StatelessWidget{
+  StationListPage(this.direction);
+
+  String direction;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('출발역'),
+        title: Text(direction),
+        centerTitle: true,
       ),
     );
   }
