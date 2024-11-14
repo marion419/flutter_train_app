50b25e3f3f6e4e4dd0afda84a57a05ec7080b2e8
marion419 <daynvi@gmail.com>
2024-11-14 11:08:23 +0900

    SeatPage 클래스  생성

---
+++ lib/pages/seat/seat_page.dart
@@ -0,0 +1,17 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class SeatPage extends StatefulWidget{
+  @override
+  State<SeatPage> createState() => _SeatPageState();
+}
+
+class _SeatPageState extends State<SeatPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('seatPage');
+  }
+
+}
