3ab5983c0c0e5099454306ad1b84eddd8756e8cf
marion419 <daynvi@gmail.com>
2024-11-14 20:22:48 +0900

    station_list_page appbar 제작

--- lib/pages/station_list/station_list_page.dart
+++ lib/pages/station_list/station_list_page.dart
@@ -5,6 +5,10 @@ import 'package:flutter/material.dart';
 class StationListPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Text('stationListPage');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('출발역'),
+      ),
+    );
   }
 }
