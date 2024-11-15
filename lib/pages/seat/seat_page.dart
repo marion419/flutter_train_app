f2472f240ac02b0bfea7b62d799d04e3336d9aca
marion419 <daynvi@gmail.com>
2024-11-15 18:38:46 +0900

    SeatPage 상단 정보 구현

--- lib/pages/seat/seat_page.dart
+++ lib/pages/seat/seat_page.dart
@@ -11,6 +11,69 @@ class SeatPage extends StatefulWidget{
 class _SeatPageState extends State<SeatPage> {
   @override
   Widget build(BuildContext context) {
-    return Text('seatPage');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('좌석 선택'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          // 역 이름
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text('출발역',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).highlightColor,
+                ),
+              ),
+              Icon(Icons.arrow_circle_right_outlined, size: 30,),
+              Text('도착역',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).highlightColor,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10,),
+          // 좌석 상태 박스
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 24,
+                height: 24,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Theme.of(context).highlightColor,
+                ),
+              ),
+              SizedBox(width: 4,),
+              Text('선택됨',),
+              SizedBox(width: 20,),
+              Container(
+                width: 24,
+                height: 24,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.grey[300]!,
+                ),
+              ),
+              Text('선택 안 됨'),
+            ],
+          )
+          // 행번호
+
+          // 좌석 리스트뷰
+
+          // 예매하기 버튼
+
+        ],
+      ),
+    );
   }
 }
