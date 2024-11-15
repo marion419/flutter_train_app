084e913ac825916fe00905f78c264af462a3a35c
marion419 <daynvi@gmail.com>
2024-11-15 20:31:59 +0900

    좌석 버튼 위젯으로 분리하기 위해 Row 삭제

--- lib/pages/seat/Widgets/seats.dart
+++ lib/pages/seat/Widgets/seats.dart
@@ -11,42 +11,30 @@ class _SeatsState extends State<Seats>{
   bool isPressed=false;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          width: 50,
-          height: 50,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            color: isPressed ? Theme.of(context).highlightColor : Colors.grey[300]!
-          ),
-          child: TextButton(
-            onPressed: (){
-              if(isPressed){
-                setState(() {
-                  isPressed=false;
-                });
-              }
-              else{
-                setState(() {
-                  isPressed=true;
-                });
-              }
-            },
-            child: 
-              Text('')
-            ,
-          ),
-        ),
-        SizedBox(width: 4,),
-        Container(
-          width: 50,
-          height: 50,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-          ),
-        ),
-      ],
+    return Container(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: isPressed ? Theme.of(context).highlightColor : Colors.grey[300]!
+      ),
+      child: TextButton(
+        onPressed: (){
+          if(isPressed){
+            setState(() {
+              isPressed=false;
+            });
+          }
+          else{
+            setState(() {
+              isPressed=true;
+            });
+          }
+        },
+        child: 
+          Text('')
+        ,
+      ),
     );
   }
 }
