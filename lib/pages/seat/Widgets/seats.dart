b15e871973946b50f1213178d5ab7fcbe4404d46
marion419 <daynvi@gmail.com>
2024-11-15 20:22:25 +0900

    좌석 위젯 생성

---
+++ lib/pages/seat/Widgets/seats.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class Seats extends StatefulWidget {
+  const Seats({super.key});
+
+  @override
+  State<Seats> createState() => _SeatsState();
+}
+
+class _SeatsState extends State<Seats>{
+  bool isPressed=false;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: isPressed ? Theme.of(context).highlightColor : Colors.grey[300]!
+          ),
+          onPressed: (){
+            if(isPressed){
+              setState(() {
+                isPressed=false;
+              });
+            }
+            else{
+              setState(() {
+                isPressed=true;
+              });
+            }
+          },
+          child: 
+            Text('')
+          ,
+        ),
+        SizedBox(width: 4,),
+        Container(
+          width: 50,
+          height: 50,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+          ),
+        ),
+      ],
+    );
+  }
+}
