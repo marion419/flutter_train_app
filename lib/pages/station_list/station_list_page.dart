3f6c740555114567d0f289d173da2aa9715a4119
marion419 <daynvi@gmail.com>
2024-11-18 20:37:13 +0900

    역 리스트 ListView.builder로 변경

--- lib/pages/station_list/station_list_page.dart
+++ lib/pages/station_list/station_list_page.dart
@@ -18,6 +18,8 @@ class _StationListPage extends State<StationListPage>{
   String direction='';
   String station='';
 
+  final List<String> stations=["수서","동탄","평택지제","천안아산","오송","대전","김천구미","동대구","경주","울산","부산"];
+
   @override
   void initState() {
     super.initState();
@@ -26,27 +28,17 @@ class _StationListPage extends State<StationListPage>{
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(direction),
-        centerTitle: true,
-      ),
-      body: 
-      ListView(
-        children: [
-          StationSelect('수서'),
-          StationSelect('동탄'),
-          StationSelect('평택지제'),
-          StationSelect('천안아산'),
-          StationSelect('오송'),
-          StationSelect('대전'),
-          StationSelect('김천구미'),
-          StationSelect('동대구'),
-          StationSelect('경주'),
-          StationSelect('울산'),
-          StationSelect('부산'),
-        ],
-      ),
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: stations.length,
+      itemBuilder: (BuildContext context, int index){
+        return Container(
+          height: 50,
+          decoration: BoxDecoration(
+            border: Border(bottom: BorderSide())
+          ),
+        );
+      },
     );
   }
 
